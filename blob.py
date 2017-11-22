e3d37c769f0c1a12aa12bc9278512c3ddf643bbd
Paul <palu.maynard@gmail.com>
2017-11-22 15:13:09 -0500

    blob.py (Lily)

--- blob.py
+++ blob.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 class Blob:
     """
     Represents a blob.
@@ -8,31 +11,39 @@ class Blob:
         Constructs an empty blob.
         """
 
-        self._P = ...  # number of pixels
-        self._x = ...  # x-coordinate of center of mass
-        self._y = ...  # y-coordinate of center of mass
+        self._P = 0  # number of pixels
+        self._x = 0  # x-coordinate of center of mass
+        self._y = 0  # y-coordinate of center of mass
 
     def add(self, i, j):
         """
         Adds pixel (i, j) to this blob.
         """
 
-        ...
+		oldx = self._P * self._x
+		self._x = (oldx + i) / (self._P + 1)	
+
+		oldy = self._P * self._y
+		self._y = (oldy + j) / (self._P + 1)
+        
+		self._P += 1
 
     def mass(self):
         """
         Returns the number of pixels added to this blob, ie, its mass.
         """
-
-        ...
+		return self._P
+        
 
     def distanceTo(self, other):
         """
         Returns the Euclidean distance between the center of mass of this blob
         and the center of mass of other blob.
         """
-
-        ...
+		dx = self._x - other._x
+		dy = self._y - other._y
+		return sqrt(dx**2 + dy**2)  # the 
+        
 
     def __str__(self):
         """
