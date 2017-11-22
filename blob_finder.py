65838fde2e0d3934567f50d6981b59fb245a540f
Paul <palu.maynard@gmail.com>
2017-11-22 15:11:36 -0500

    blob_finder.py initial code

--- blob_finder.py
+++ blob_finder.py
@@ -18,17 +18,25 @@ class BlobFinder:
         """
 
         # Initialize an empty list for the blobs in pic.
-        self._blobs = ...
+        self._blobs = []
 
         # Create a 2D list of booleans called marked, having the same
         # dimensions as pic.
-        ...
+        
+        x = pic.height()
+        y = pic.width()
+        marked = stdarray.create2D(x, y, False)
 
         # Enumerate the pixels of pic, and for each pixel (i, j):
         # 1. Create a Blob object called blob.
         # 2. Call _findBlob() with the right arguments.
         # 3. Add blob to _blobs if it has a non-zero mass.
-        ...
+        for i in range(x):
+            for j in range(y):
+                blob = Blob()
+                self._findBlob(pic, tau, i, j, blob)
+                if blob.mass() > 0:
+                    self._blobs.append(blob)
 
     def _findBlob(self, pic, tau, i, j, marked, blob):
         """
@@ -40,30 +48,43 @@ class BlobFinder:
 
         # Base case: return if pixel (i, j) is out of bounds, or if it
         # is marked, or if its luminance is less than tau.
-        ...
+        if i < 0 or i >= pic.height():
+            return
+        if i < 0 or i >= pic.width():
+            return
+        if marked[i][j]:
+            return
+        if luminance.luminance(pic.get(i, j)) < tau:
+            return
 
         # Mark the pixel.
-        ...
+        marked[i][j] = True
 
         # Add the pixel to blob.
-        ...
+        blob.add(i, j)
 
         # Recursively call _findBlob() on the N, E, W, S pixels.
-        ...
+        self._findBlob(pic, tau, i + 1, j, marked, blob) # South
+        self._findBlob(pic, tau, i, j + 1, marked, blob) # East
+        self._findBlob(pic, tau, i, j - 1, marked, blob) # West
+        self._findBlob(pic, tau, i - 1, j, marked, blob) # North
 
     def getBeads(self, P):
         """
         Returns a list of all beads with >= P pixels.
         """
 
-        ...
+        return [blob for blob in self.__blobs if blob.mass() >= P]
 
 
 # Takes an integer P, a float tau, and the name of a JPEG file as
 # command-line arguments; writes out all of the beads with at least P
 # pixels; and then writes out all of the blobs (beads with at least 1 pixel).
 def _main():
-    ...
+    P = int(sys.argv[0])
+    tau = float(sys.argv[1])
+    pic = Picture(sys.argv[2])
+    
 
 if __name__ == '__main__':
     _main()
