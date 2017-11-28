060c580b03cb000eb3c5e18f969f31efb707cbe9
Paul <palu.maynard@gmail.com>
2017-11-28 15:19:02 -0500

    Moved bead tracker with visualization into a separate program

---
+++ bead_trackerv.py
@@ -0,0 +1,40 @@
+import stdio
+import sys
+from blob_finder import BlobFinder
+from picture import Picture
+import stddraw
+
+
+# Takes an integer P, a float tau, a float delta, and a sequence of JPEG
+# filenames as command-line arguments; identifies the beads in each JPEG
+# image using BlobFinder; and writes out (one per line, formatted with 4
+# decimal places to the right of decimal point) the radial distance that
+# each bead moves from one frame to the next (assuming it is no more than
+# delta).
+def main():
+    stddraw.setCanvasSize(640, 480)
+    
+    P = int(sys.argv[1])
+    tau = float(sys.argv[2])
+    delta = float(sys.argv[3])
+    files = sys.argv[4:]
+
+    stddraw.picture(Picture(files[0]))
+    stddraw.show(0)
+    
+    beads1 = BlobFinder(Picture(files[0]), tau).getBeads(P)
+    
+    for i in range(1, len(files)):
+        stddraw.picture(Picture(files[i]))
+        stddraw.show(0)
+
+        beads0 = beads1
+        beads1 = BlobFinder(Picture(files[i]), tau).getBeads(P)
+        for bead in beads0:
+            dist = min(bead.distanceTo(other) for other in beads1)
+            if dist <= delta:
+                stdio.writef('%.4f\n', dist)
+        stdio.writeln()
+
+if __name__ == '__main__':
+    main()
