096850fb1da253f9aa261e45592c552379db8940
Paul <palu.maynard@gmail.com>
2017-11-28 14:43:06 -0500

    Thresholding in animator

--- play.py
+++ play.py
@@ -1,19 +1,25 @@
 import stddraw
 from picture import Picture
 import sys
+from threshold import threshold
 
-if len(sys.argv) != 3:
+if len(sys.argv) < 3:
     print """
     Usage:
-    python %s <run #> <frame delay)
+    python %s <run #> <frame delay> [<threshold>]
     """ % sys.argv[0]
     sys.exit()
 
 stddraw.setCanvasSize(640, 480)
 run = int(sys.argv[1])
 delay = int(sys.argv[2])
+tau = None
+if len(sys.argv) > 3:
+    tau = float(sys.argv[3])
 
 for frame in range(200):
     pic = Picture('data/run_%d/frame%05d.jpg' % (run, frame))
+    if tau:
+        threshold(pic, tau)
     stddraw.picture(pic)
     stddraw.show(delay)
