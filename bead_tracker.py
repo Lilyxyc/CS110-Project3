cca85cd23186b78e1f9ae9fc536bdbaf4adbd472
Paul <palu.maynard@gmail.com>
2017-11-28 16:24:24 -0500

    Visualization to main file

--- bead_tracker.py
+++ bead_tracker.py
@@ -2,6 +2,7 @@ import stdio
 import sys
 from blob_finder import BlobFinder
 from picture import Picture
+import stddraw
 
 
 # Takes an integer P, a float tau, a float delta, and a sequence of JPEG
@@ -11,13 +12,22 @@ from picture import Picture
 # each bead moves from one frame to the next (assuming it is no more than
 # delta).
 def main():
+    stddraw.setCanvasSize(640, 480)
+    
     P = int(sys.argv[1])
     tau = float(sys.argv[2])
     delta = float(sys.argv[3])
     files = sys.argv[4:]
+
+    stddraw.picture(Picture(files[0]))
+    stddraw.show(0)
     
     beads1 = BlobFinder(Picture(files[0]), tau).getBeads(P)
+    
     for i in range(1, len(files)):
+        stddraw.picture(Picture(files[i]))
+        stddraw.show(0)
+
         beads0 = beads1
         beads1 = BlobFinder(Picture(files[i]), tau).getBeads(P)
         for bead in beads0:
