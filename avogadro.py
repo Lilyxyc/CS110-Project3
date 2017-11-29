ff28c63b9b12db97191aed2af622af62a020171b
Paul <palu.maynard@gmail.com>
2017-11-29 09:12:41 -0500

    Comments on avogadro.py

--- avogadro.py
+++ avogadro.py
@@ -4,25 +4,51 @@ import stdio
 # Constants:
 PXTOM = 0.175e-6    # m/px      -- Conversion Factor
 
-TEMP = 297.0        # K         -- Room temperature
-VISC = 9.135e-4     # Ns/m2     -- Viscosity
-RADIUS = 0.5e-6     # m         -- Radius
+TEMP = 297.0        # K         --   T: Room temperature
+VISC = 9.135e-4     # Ns/m2     -- eta: Viscosity
+RADIUS = 0.5e-6     # m         -- rho: Radius
 
-GASC = 8.31457      # J/K mol   -- Gas constant
+GASC = 8.31457      # J/K mol   --   R: Gas constant
 
 
 # Reads in the displacements produced by bead_tracker.py from standard
 # input; computes an estimate of Boltzmann's constant and Avogadro's number;
 # and writes those estimates to standard output.
 def main():
+    # Read displacements
     displacements = stdio.readAllFloats()
+
+    # Calculate self-diffusion constant D
+
+    #        2   r1^2 + ... + rn^2
+    # D = sig  = -----------------
+    #                   2n
+
+    # Convert displacements to meters and square
     displacements = [(r * PXTOM)**2 for r in displacements]
+
+    # Divide sum by 2 * number of displacements
     diffusion = sum(displacements)/(2 * len(displacements))
 
-    boltzman = (6.0 * math.pi * VISC * RADIUS * diffusion) / TEMP
-    stdio.writef('Boltzman = %e\n', boltzman)
+    # Calculate Boltzmann constant k
 
-    avogadro = GASC / boltzman
+    #         k T             6pi eta rho D
+    # D = ----------- ==> k = -------------
+    #     6pi eta rho               T
+
+    boltzmann = (6.0 * math.pi * VISC * RADIUS * diffusion) / TEMP
+
+    # Print Boltzman (sic) constant
+    stdio.writef('Boltzman = %e\n', boltzmann)
+
+    # Calculate Avogadro's number Na
+
+    #     R           R
+    # k = -- ==> Na = -
+    #     Na          k
+    avogadro = GASC / boltzmann
+
+    # Print Avogadro's number
     stdio.writef('Avogadro = %e\n', avogadro)
 
 if __name__ == '__main__':
